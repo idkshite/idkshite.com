d9325d5e227194f3966462a0df230fb816d0543c
DATADEER <konto@datadeer.de>
2021-05-16 09:53:23 +0200

    * cleanup styles (still some stuff looks off)

---
+++ public/styles/font.ts
@@ -0,0 +1,19 @@
+export const FONT_SIZE = {
+  TITLE2: 35.2,
+  TITLE1: 28.1,
+  SMALL_TITLE1: 22,
+  SMALL_TITLE2: 18,
+  SMALL_TITLE3: 14,
+  BODY4: 22.5,
+};
+
+export const LINE_HEIGHT = {
+  BODY: 40,
+  TITLE: 30,
+};
+
+export const VERTICAL_MARGIN = {
+  DEFAULT: LINE_HEIGHT.BODY,
+  DOUBLE: LINE_HEIGHT.BODY * 2,
+  HALF: LINE_HEIGHT.BODY / 2,
+};
--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "../../public/styles/content.module.css";
 import Author from "./Author";
 import Copyright from "./Copyright";
 import Date from "./Date";
@@ -12,6 +11,7 @@ import { SocialList } from "./SocialList";
 import TagButton from "./TagButton";
 import { getAuthor } from "../lib/authors";
 import { getTag } from "../lib/tags";
+import PostContent from "./PostContent";
 
 type Props = {
   title: string;
@@ -31,7 +31,7 @@ export default function PostLayout({
   description = "",
   children,
 }: Props) {
-  const keywords = tags.map(it => getTag(it).name);
+  const keywords = tags.map((it) => getTag(it).name);
   const authorName = getAuthor(author).name;
   return (
     <Layout>
@@ -72,7 +72,7 @@ export default function PostLayout({
               </div>
             </div>
           </header>
-          <div className={styles.content}>{children}</div>
+          <PostContent>{children}</PostContent>
           <ul className={"tag-list"}>
             {tags.map((it, i) => (
               <li key={i}>
@@ -90,148 +90,148 @@ export default function PostLayout({
       </div>
       <style jsx>
         {`
-            .container {
-              display: block;
-              max-width: 36rem;
-              width: 100%;
-              margin: 0 auto;
-              padding: 0 1.5rem;
-              box-sizing: border-box;
-              z-index: 0;
-            }
-            .metadata div {
-              display: inline-block;
-              margin-right: 0.5rem;
-            }
-            article {
-              flex: 1 0 auto;
-            }
-            h1 {
-              margin: 0 0 0.5rem;
-              font-size: 2.25rem;
-            }
-            .tag-list {
-              list-style: none;
-              text-align: right;
-              margin: 1.75rem 0 0 0;
-              padding: 0;
-            }
-            .tag-list li {
-              display: inline-block;
-              margin-left: 0.5rem;
-            }
-            .social-list {
-              margin-top: 3rem;
-              text-align: center;
-            }
+          .container {
+            display: block;
+            max-width: 36rem;
+            width: 100%;
+            margin: 0 auto;
+            padding: 0 1.5rem;
+            box-sizing: border-box;
+            z-index: 0;
+          }
+          .metadata div {
+            display: inline-block;
+            margin-right: 0.5rem;
+          }
+          article {
+            flex: 1 0 auto;
+          }
+          h1 {
+            margin: 0 0 0.5rem;
+            font-size: 2.25rem;
+          }
+          .tag-list {
+            list-style: none;
+            text-align: right;
+            margin: 1.75rem 0 0 0;
+            padding: 0;
+          }
+          .tag-list li {
+            display: inline-block;
+            margin-left: 0.5rem;
+          }
+          .social-list {
+            margin-top: 3rem;
+            text-align: center;
+          }
 
-            @media (min-width: 769px) {
-              .container {
-                display: flex;
-                flex-direction: column;
-              }
+          @media (min-width: 769px) {
+            .container {
+              display: flex;
+              flex-direction: column;
             }
-          `}
+          }
+        `}
       </style>
       <style global jsx>
         {`
-            /* Syntax highlighting */
-            .token.comment,
-            .token.prolog,
-            .token.doctype,
-            .token.cdata,
-            .token.plain-text {
-              color: #6a737d;
-            }
+          /* Syntax highlighting */
+          .token.comment,
+          .token.prolog,
+          .token.doctype,
+          .token.cdata,
+          .token.plain-text {
+            color: #6a737d;
+          }
 
-            .token.atrule,
-            .token.attr-value,
-            .token.keyword,
-            .token.operator {
-              color: #d73a49;
-            }
+          .token.atrule,
+          .token.attr-value,
+          .token.keyword,
+          .token.operator {
+            color: #d73a49;
+          }
 
-            .token.property,
-            .token.tag,
-            .token.boolean,
-            .token.number,
-            .token.constant,
-            .token.symbol,
-            .token.deleted {
-              color: #22863a;
-            }
+          .token.property,
+          .token.tag,
+          .token.boolean,
+          .token.number,
+          .token.constant,
+          .token.symbol,
+          .token.deleted {
+            color: #22863a;
+          }
 
-            .token.selector,
-            .token.attr-name,
-            .token.string,
-            .token.char,
-            .token.builtin,
-            .token.inserted {
-              color: #032f62;
-            }
+          .token.selector,
+          .token.attr-name,
+          .token.string,
+          .token.char,
+          .token.builtin,
+          .token.inserted {
+            color: #032f62;
+          }
 
-            .token.function,
-            .token.class-name {
-              color: #6f42c1;
-            }
+          .token.function,
+          .token.class-name {
+            color: #6f42c1;
+          }
 
-            /* language-specific */
+          /* language-specific */
 
-            /* JSX */
-            .language-jsx .token.punctuation,
-            .language-jsx .token.tag .token.punctuation,
-            .language-jsx .token.tag .token.script,
-            .language-jsx .token.plain-text {
-              color: #24292e;
-            }
+          /* JSX */
+          .language-jsx .token.punctuation,
+          .language-jsx .token.tag .token.punctuation,
+          .language-jsx .token.tag .token.script,
+          .language-jsx .token.plain-text {
+            color: #24292e;
+          }
 
-            .language-jsx .token.tag .token.attr-name {
-              color: #6f42c1;
-            }
+          .language-jsx .token.tag .token.attr-name {
+            color: #6f42c1;
+          }
 
-            .language-jsx .token.tag .token.class-name {
-              color: #005cc5;
-            }
+          .language-jsx .token.tag .token.class-name {
+            color: #005cc5;
+          }
 
-            .language-jsx .token.tag .token.script-punctuation,
-            .language-jsx .token.attr-value .token.punctuation:first-child {
-              color: #d73a49;
-            }
+          .language-jsx .token.tag .token.script-punctuation,
+          .language-jsx .token.attr-value .token.punctuation:first-child {
+            color: #d73a49;
+          }
 
-            .language-jsx .token.attr-value {
-              color: #032f62;
-            }
+          .language-jsx .token.attr-value {
+            color: #032f62;
+          }
 
-            .language-jsx span[class="comment"] {
-              color: pink;
-            }
+          .language-jsx span[class="comment"] {
+            color: pink;
+          }
 
-            /* HTML */
-            .language-html .token.tag .token.punctuation {
-              color: #24292e;
-            }
+          /* HTML */
+          .language-html .token.tag .token.punctuation {
+            color: #24292e;
+          }
 
-            .language-html .token.tag .token.attr-name {
-              color: #6f42c1;
-            }
+          .language-html .token.tag .token.attr-name {
+            color: #6f42c1;
+          }
 
-            .language-html .token.tag .token.attr-value,
-            .language-html
-              .token.tag
-              .token.attr-value
-              .token.punctuation:not(:first-child) {
-              color: #032f62;
-            }
+          .language-html .token.tag .token.attr-value,
+          .language-html
+            .token.tag
+            .token.attr-value
+            .token.punctuation:not(:first-child) {
+            color: #032f62;
+          }
 
-            /* CSS */
-            .language-css .token.selector {
-              color: #6f42c1;
-            }
+          /* CSS */
+          .language-css .token.selector {
+            color: #6f42c1;
+          }
 
-            .language-css .token.property {
-              color: #005cc5;
-            }
-          `}
+          .language-css .token.property {
+            color: #005cc5;
+          }
+        `}
       </style>
     </Layout>
   );
