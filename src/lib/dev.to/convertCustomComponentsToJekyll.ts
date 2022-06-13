d1aa3a59d60bce105996d0009ad12c0686f59919
idkshite <mail@idkshite.com>
2022-06-13 08:31:55 +0200

    slightly broken cross posting

--- src/lib/dev.to/convertCustomComponentsToJekyll.ts
+++ src/lib/dev.to/convertCustomComponentsToJekyll.ts
@@ -1,62 +1,243 @@
-import {CustomMDXComponentName} from "../../pages/posts/[post]";
+import { CustomMDXComponentName } from "../../pages/posts/[post]";
+import fs from "node:fs/promises";
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { toMarkdown } from "mdast-util-to-markdown";
+import { mdxFromMarkdown, mdxToMarkdown } from "mdast-util-mdx";
+import { Content, Root } from "mdast-util-from-markdown/lib";
+import { mdxjs } from "micromark-extension-mdxjs";
+import flattendeep from "lodash.flattendeep";
 
-export function convertCustomComponentsToJekyll(content: string):string {
-    let convertedContent = content
+type CustomMDXTag = {
+  expression: string;
+  position: { start: number; end: number };
+  tagName: string;
+  attributes: CustomMDXTagAttributes;
+  content: string;
+};
+type CustomMDXTagAttributes = { [key in string]: string | number };
+type CustomMDXComponent = {
+  tagName: string;
+  tokenEqualsThisTag: (token: Content) => boolean;
+};
+
+const LinkMDXComponent = {
+  tagName: "Link",
+  tokenEqualsThisTag: findTokenByNameFunction("Link"),
+};
+
+const ReplitMDXComponent = {
+  tagName: "Replit",
+  tokenEqualsThisTag: findTokenByNameFunction("Replit"),
+};
+
+const ImgWithTextMDXComponent = {
+  tagName: "ImgWithText",
+  tokenEqualsThisTag: findTokenByNameFunction("ImgWithText"),
+};
+
+function findTokenByNameFunction(tagName) {
+  return (token) => token.name === tagName;
+}
+
+function getTokenChild(token) {
+  if (token?.children) {
+    return [token, getTokenChild(token?.children)];
+  }
+  return token;
+}
+
+export function convertCustomComponentsToJekyll(markdown: string) {
+  const flatFileTokens = flattendeep(
+    getFileTree(markdown).children.reduce<Content[]>((allTokens, token) => {
+      return [...allTokens, getTokenChild(token)];
+    }, [])
+  ).filter(
+    (token) =>
+      token.type === "mdxJsxFlowElement" || token.type === "mdxJsxTextElement"
+  );
+
+  // console.log("flatFileTokens", flatFileTokens);
+
+  // TODO: make this dry
+  let convertedContent = markdown;
+  convertedContent = replaceComponent({
+    flatFileTokens,
+    markdown: convertedContent,
+    componentToReplace: LinkMDXComponent,
+    replaceWith: (tag) => `[${tag.content}](${tag.attributes.url})`,
+  });
+  convertedContent = replaceComponent({
+    flatFileTokens,
+    markdown: convertedContent,
+    componentToReplace: ReplitMDXComponent,
+    replaceWith: (tag) => {
+      return `{% replit ${tag.attributes.url} %}`;
+    },
+  });
+  convertedContent = replaceComponent({
+    flatFileTokens,
+    markdown: convertedContent,
+    componentToReplace: ImgWithTextMDXComponent,
+    replaceWith: (tag) => {
+      return `![${tag.content ?? "no alt tag provided"}](${
+        tag.attributes.url
+      }.jpg)`;
+    },
+  });
+  return convertedContent;
+
+  /*let convertedContent = content
     convertedContent = replaceComponent({name:"Link", tagName: "Link", selfClosing: false} , convertedContent, (tag) => `[${tag.content}](${tag.attributes.url})`);
     return replaceComponent({name: "Replit", tagName: "Replit", selfClosing: true }, convertedContent, (tag) => `{% replit ${tag.attributes.url} %}`);
-    // return replaceComponent({name: "ImgWithText", tagName: "ImgWithText", selfClosing: false } , convertedContent, (tag) => `![${tag.attributes?.alt ?? "no alt tag provided"}](${tag.attributes.url}.jpg)`);
+    // return replaceComponent({name: "ImgWithText", tagName: "ImgWithText", selfClosing: false } , convertedContent, (tag) => `![${tag.attributes?.alt ?? "no alt tag provided"}](${tag.attributes.url}.jpg)`);*/
 }
 
-function replaceComponent(componentType: CustomMDXComponent, content, replaceWith: (tag: CustomMDXTag) => string){
-    const allTags = getTagsByType(componentType,content);
-    if(!allTags) {
-        console.log(`Didn't find any tags for: ${componentType.name}'s`);
-        return content
-    }
-
-    return allTags.reduce((newContent, tag) => {
-        return newContent.replace(tag.expression, replaceWith(tag));
-    },content)
+function getFileTree(content: string) {
+  return fromMarkdown(content, {
+    extensions: [mdxjs()],
+    mdastExtensions: [mdxFromMarkdown()],
+  });
 }
 
-type CustomMDXTag = {expression: string, tag: string, attributes:CustomMDXTagAttributes , content: string}
-type CustomMDXTagAttributes = { [key in string]: string | number }
-type CustomMDXComponent = {name: CustomMDXComponentName, tagName: string, selfClosing: boolean }
-
-function getTagsByType(componentType: CustomMDXComponent, content: string): CustomMDXTag[]{
-    try {
-        let pattern = `<(${componentType.tagName}+)([^<]+)*(?:>(.*)<\/${componentType.tagName}>|\s+\/>)`;
-        if(componentType.selfClosing) pattern = `<(${componentType.tagName}+)([^<]+)\/>`;
-
-        const regexSingleTag = new RegExp(pattern);
-        const regexAllTags = new RegExp(pattern, "g");
-
-        const matches = content.match(regexAllTags);
-        if(!matches) return null;
-
-        return matches.map((link) => {
-            const content = link.match(regexSingleTag);
-            console.log("content",content);
-            return {
-                expression: content[0],
-                tag: content[1],
-                attributes: parseAttributes(content[2]),
-                content: content[3]
-            } as CustomMDXTag
-        });
-
-    }catch(error){
-        console.error(error);
-    }
+function getTokenByCondition({
+  flatFileTokens,
+  predicateFunction,
+}: {
+  flatFileTokens: Content[];
+  predicateFunction: (token: Content) => boolean;
+}) {
+  return flatFileTokens.filter((token) => {
+    return predicateFunction(token);
+  });
 }
 
-function parseAttributes(attributes:string): CustomMDXTagAttributes {
-    const attributeKeyValueRegex = /(.*)\={?"+(.*)["}||}||"]/
-    return attributes
-        .trim()
-        .split(" ").reduce((sum, curr) => {
-            const match = curr.match(attributeKeyValueRegex);
-            return {...sum,[match[1]]: match[2] }
-        },{});
+function replaceComponent({
+  flatFileTokens,
+  markdown,
+  componentToReplace,
+  replaceWith,
+}: {
+  flatFileTokens: Content[];
+  markdown: string;
+  componentToReplace: CustomMDXComponent;
+  replaceWith: (tag: CustomMDXTag) => string;
+}) {
+  const tokens = getTokenByCondition({
+    flatFileTokens,
+    predicateFunction: componentToReplace.tokenEqualsThisTag,
+  });
+
+  if (!tokens) {
+    console.log(
+      `Didn't find any tokens for tag: ${componentToReplace.tagName}'s`
+    );
+    return markdown;
+  }
+
+  //console.log(`found ${tokens.length} ${componentToReplace.tagName} tokens`);
+  //console.log(tokens);
+
+  return tokens
+    .map((token) => convertTokenToTag(token, markdown))
+    .reduce((newMarkdown, tag, index) => {
+      // TODO: replace by position for more reliable results
+      const replacedMarkdown = replaceTag({
+        markdown: newMarkdown,
+        tag,
+        replaceWith,
+      });
+      // console.log("replacedMarkdown", replacedMarkdown, "INDEX: ", index);
+      return (newMarkdown = replacedMarkdown);
+    }, markdown);
+
+  /*return tokens.reduce((newContent, tag) => {
+    return newContent.replace(tag.expression, replaceWith(tag));
+  }, content);*/
 }
 
+function replaceTag({
+  markdown,
+  tag,
+  replaceWith,
+}: {
+  markdown: string;
+  tag: CustomMDXTag;
+  replaceWith: (tag: CustomMDXTag) => string;
+}) {
+  // console.log("tag", tag.expression, "replace with ", replaceWith(tag));
+  return markdown.replace(tag.expression, replaceWith(tag));
+}
+
+/*function replaceTagByPosition({
+  markdown,
+  tag,
+  replaceWith,
+}: {
+  markdown: string;
+  tag: CustomMDXTag;
+  replaceWith: (tag: CustomMDXTag) => string;
+}) {
+  console.log("tag", tag, "replace with ", replaceWith(tag));
+  return (
+    markdown.substring(0, tag.position.start) +
+    replaceWith(tag) +
+    markdown.substring(tag.position.end + 1)
+  );
+}*/
+
+// TODO: fix token type
+function convertTokenToTag(token, markdown): CustomMDXTag {
+  const startPos = token.position.start.offset;
+  const endPos = token.position.end.offset;
+  console.log(
+    "\n\n SUBSTRING",
+    startPos,
+    "until",
+    endPos,
+    "is: ",
+    markdown.substring(startPos, endPos + 1),
+    "\n\n"
+  );
+  return {
+    expression: markdown.substring(startPos, endPos + 1),
+    position: {
+      start: startPos,
+      end: endPos,
+    },
+    tagName: token.name,
+    attributes: getAttributes(token),
+    content: getContent(token),
+  };
+}
+
+function getAttributes(token) {
+  if (!token?.attributes) return {};
+  return token.attributes.reduce((attributes, prevAttribute) => {
+    // console.log("prevAttribute?.value", getAttributeValue(prevAttribute));
+    return {
+      ...attributes,
+      [prevAttribute.name]: getAttributeValue(prevAttribute),
+    };
+  }, {});
+}
+
+function getAttributeValue(attribute) {
+  // value isn't always inside value.value but also sometimes just as a string in value. idk why
+  if (attribute?.value?.value)
+    return attribute.value.value.replace(/['"]+/g, "").trim();
+  if (attribute?.value) return attribute?.value.replace(/['"]+/g, "").trim();
+  return "";
+}
+
+function getContent(token) {
+  //console.log(`${token.name} has children ${JSON.stringify(token?.children)}`);
+  if (!token?.children) return null;
+  const flattenedChildrenWithValue = flattendeep(getTokenChild(token)).filter(
+    (token) => token?.value
+  );
+  // console.log("flattenedChildren", JSON.stringify(flattenedChildrenWithValue));
+
+  return flattenedChildrenWithValue.reduce((content, child) => {
+    return content + " " + child.value.trim();
+  }, "");
+}
--- src/pages/api/publish/[postSlug].ts
+++ src/pages/api/publish/[postSlug].ts
@@ -1,70 +1,61 @@
-import {createDevToPost} from "../../../lib/dev.to/createPost";
+import { createDevToPost } from "../../../lib/dev.to/createPost";
 import fs from "fs";
 import matter from "gray-matter";
 import yaml from "js-yaml";
-import {serialize} from "next-mdx-remote/serialize";
-import {MDXRemote} from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
 import YouTube from "react-youtube";
-import {TwitterTweetEmbed} from "react-twitter-embed";
-import {CodeSandbox} from "../../../components/rich-content/CodeSandbox";
-import {Replit} from "../../../components/rich-content/Replit";
-import {Video} from "../../../components/rich-content/Video";
-import {Link} from "../../../components/rich-content/Link";
-import {Imgur} from "../../../components/rich-content/Image";
-import {ImgWithText} from "../../../components/rich-content/ImageWithText";
-import {convertCustomComponentsToJekyll} from "../../../lib/dev.to/convertCustomComponentsToJekyll";
-import {slugToPostContent} from "../../../lib/posts";
+import { TwitterTweetEmbed } from "react-twitter-embed";
+import { CodeSandbox } from "../../../components/rich-content/CodeSandbox";
+import { Replit } from "../../../components/rich-content/Replit";
+import { Video } from "../../../components/rich-content/Video";
+import { Link } from "../../../components/rich-content/Link";
+import { Imgur } from "../../../components/rich-content/Image";
+import { ImgWithText } from "../../../components/rich-content/ImageWithText";
+import { convertCustomComponentsToJekyll } from "../../../lib/dev.to/convertCustomComponentsToJekyll";
+import { slugToPostContent } from "../../../lib/posts";
 
 export type PostFrontMatter = {
-
-    slug: string // 'asana-node-cli',
-    title: string // 'Conjure your asana tasks straight onto your terminal window',
-    subtitle: string // 'Quickly build an asana CLI with node.js (oclif.io)',
-    date: string // '2022-05-31',
-    author: string // 'lucca',
-    tags: string[] // [ 'programming', 'learn-in-public' ]
-    cover_image?: string
-
-}
+  slug: string; // 'asana-node-cli',
+  title: string; // 'Conjure your asana tasks straight onto your terminal window',
+  subtitle: string; // 'Quickly build an asana CLI with node.js (oclif.io)',
+  date: string; // '2022-05-31',
+  author: string; // 'lucca',
+  tags: string[]; // [ 'programming', 'learn-in-public' ]
+  cover_image?: string;
+};
 
 export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const slug = req?.query?.postSlug;
+    if (!slug || !slug.trim())
+      return res.status(400).send("missing post slug query parameter");
+    const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
+    const { content, data } = matter(source, {
+      engines: {
+        yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
+      },
+    });
+    const frontMatter = data as PostFrontMatter;
 
-    if (req.method === 'GET') {
-        const slug = req?.query?.postSlug;
-        if(!slug || !slug.trim()) return res.status(400).send("missing post slug query parameter");
-        const source = fs.readFileSync(slugToPostContent[slug].fullPath, "utf8");
-        const { content, data } = matter(source, {
-            engines: {
-                yaml: (s) => yaml.load(s, { schema: yaml.JSON_SCHEMA }) as object,
-            },
-        });
-        const frontMatter = data as PostFrontMatter;
+    const cleanedContent = convertCustomComponentsToJekyll(content);
 
-        const cleanedContent = convertCustomComponentsToJekyll(content);
-
-        let result;
-        try {
-            result = await createDevToPost({slug,markdown: cleanedContent,frontMatter})
-        }catch(error){
-            console.error(error);
-            return res.status(500).send("error: couldn't publish the post. check logs for more details");
-        }
-
-        return res.status(200).send(`published: ${slug}`);
-    } else {
-        return res.status(405).send("only GET request is valid");
+    let result;
+    try {
+      result = await createDevToPost({
+        slug,
+        markdown: cleanedContent,
+        frontMatter,
+      });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .send("error: couldn't publish the post. check logs for more details");
     }
-}
 
-const getCircularReplacer = () => {
-    const seen = new WeakSet()
-    return (key, value) => {
-        if (typeof value === "object" && value !== null) {
-            if (seen.has(value)) {
-                return
-            }
-            seen.add(value)
-        }
-        return value
-    }
+    return res.status(200).send(`published: ${slug}`);
+  } else {
+    return res.status(405).send("only GET request is valid");
+  }
 }
