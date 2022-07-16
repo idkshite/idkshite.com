1dfd285deaa6f890384a9188d994bfd068c8b391
idkshite <mail@idkshite.com>
2022-07-16 11:30:08 +0200

    add controls on hover

--- src/components/rich-content/Video.tsx
+++ src/components/rich-content/Video.tsx
@@ -1,33 +1,41 @@
-import {useVideo} from "react-use";
-import {useEffect, useState} from "react";
+import { useVideo } from "react-use";
+import { useEffect, useState } from "react";
 
-export function Video({src}:{src:string}){
-    const [video, state, controls, ref] = useVideo(<video playsInline autoPlay muted loop>
-        <source src={src} />
-    </video>)
+export function Video({ src }: { src: string }) {
+  const [progress, setProgress] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
-    const [progress, setProgress] = useState(0);
+  const [video, state, controls, ref] = useVideo(
+    <video playsInline autoPlay muted loop controls={isHovered}>
+      <source src={src} />
+    </video>
+  );
 
-    useEffect(() => {
-        setProgress(getVideoProgressInPercent(ref?.current?.duration, state.time))
-    },[state])
+  useEffect(() => {
+    setProgress(getVideoProgressInPercent(ref?.current?.duration, state.time));
+  }, [state]);
 
-    return (
-        <div className={"rich-content"}>
-            {video}
-            <div className="w-full bg-gray-300">
-                <div className="bg-gray-600 h-[4px]" style={{
-                    width: `${progress}%`,
-                    transition: "width 300ms",
-                }}/>
-            </div>
-        </div>
-
-    )
+  return (
+    <div
+      className={"rich-content"}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {video}
+      <div className="w-full bg-gray-300">
+        <div
+          className="bg-gray-600 h-[4px]"
+          style={{
+            width: `${progress}%`,
+            transition: "width 300ms",
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 function getVideoProgressInPercent(duration, time) {
-    console.log(duration, time);
-    if (duration === 0 || time === 0) return 0;
-    return (Math.floor(time) / Math.floor(duration)) * 100
+  if (duration === 0 || time === 0) return 0;
+  return (Math.floor(time) / Math.floor(duration)) * 100;
 }
